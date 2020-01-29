dd6cabe40b0c166956f1b549e1a8e3ce1568b6d3
aashnamsft <aagarg@microsoft.com>
2020-01-29 11:08:19 -0800

    minor changes based on PR comments

--- azure/petctl.py
+++ azure/petctl.py
@@ -3,58 +3,57 @@ import os
 import subprocess
 import yaml
 import json
+import uuid
 
-# +
-def configure_yaml(yaml_file, petctl_dir,args):
-    
+
+def configure_yaml(yaml_file, petctl_dir, args):
     print('Configuring azure-pytorch-elastic.yaml....')
-    
+
     with open(yaml_file) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
-    
-    data["spec"]["parallelism"] = args.max_size
-    data["spec"]["template"]["spec"]["containers"][0]["env"].extend([dict([('name', 'JOB_ID'), ('value', args.name)]),
-                                                                     dict([('name', 'MIN_SIZE'), ('value', str(args.min_size))]),
-                                                                     dict([('name', 'MAX_SIZE'), ('value', str(args.max_size))])])
-    result_yaml = open(os.path.join(petctl_dir, "azure-pytorch-elastic.yaml"),"w")
-    yaml.dump(data,result_yaml)
 
-    
-def configure_json(json_file, petctl_dir,args):
-    
+    data["spec"]["parallelism"] = args.max_size
+    data["spec"]["template"]["spec"]["containers"][0]["env"].extend(
+        [dict([('name', 'JOB_ID'), ('value', str(uuid.uuid1()) + "_" + args.name)]),
+         dict([('name', 'MIN_SIZE'), ('value', str(args.min_size))]),
+         dict([('name', 'MAX_SIZE'), ('value', str(args.max_size))])])
+    result_yaml = open(os.path.join(petctl_dir, "azure-pytorch-elastic.yaml"), "w")
+    yaml.dump(data, result_yaml)
+
+
+def configure_json(json_file, petctl_dir, args):
     print('Configuring kubernetes.json file...')
-    
+
     with open(json_file) as f:
-        data =  json.load(f)
+        data = json.load(f)
     data["properties"]["masterProfile"]["count"] = 1
     data["properties"]["agentPoolProfiles"][0]["count"] = args.max_size
-    
-    result_json = open(os.path.join(petctl_dir,"kubernetes.json"),"w")
+
+    result_json = open(os.path.join(petctl_dir, "kubernetes.json"), "w")
     json.dump(data, result_json, indent=4)
-    
-    
+
+
 # Create a YAML job file based on user inputs
 def configure(args):
-    PETCTL_DIR             = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__) + 'config/'))
-    SAMPLE_YAML_FILE       = os.path.join(PETCTL_DIR, "sample_specs.yaml")
+    PETCTL_DIR = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), 'config/'))
+    SAMPLE_YAML_FILE = os.path.join(PETCTL_DIR, "sample_specs.yaml")
     KUBERNETES_JSON_FILE = os.path.join(PETCTL_DIR, "kubernetes.json")
-    
+
     configure_yaml(SAMPLE_YAML_FILE, PETCTL_DIR, args)
     configure_json(KUBERNETES_JSON_FILE, PETCTL_DIR, args)
-    
-    
 
-# +
+
 def run_job():
     subprocess.call("./run_job.sh", shell=True)
 
+
 def setup(args):
-    subprocess.call("./setup.sh " + 
+    subprocess.call("./setup.sh " +
                     args.subscription_id + " " +
                     args.dns_prefix + " " +
                     args.rg + " " +
                     args.location + " " +
-                    args.client_id + " " + 
+                    args.client_id + " " +
                     args.client_secret + " " +
                     args.storage_account_name + " " +
                     args.storage_account_key + " " +
@@ -63,107 +62,105 @@ def setup(args):
                     args.docker_password, shell=True)
 
 
-# -
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    
+
     subparser = parser.add_subparsers(
         title="actions", description="setup | configure | run job", dest="command"
     )
-    
-     # ---------------------------------- #
-     #               SETUP                #
-     # ---------------------------------- #
-        
+
+    # ---------------------------------- #
+    #               SETUP                #
+    # ---------------------------------- #
+
     parser_setup = subparser.add_parser(
         "setup", help="set up aks-engine, cluster and other dependencies"
     )
-    
+
     parser_setup.add_argument(
         "--dns_prefix",
         type=str,
         required=False,
-        default = "azure-pytorch-elastic",
+        default="azure-pytorch-elastic",
         help="Dns prefix of the app",
     )
-    
+
     parser_setup.add_argument(
         "--subscription_id",
         type=str,
         required=True,
         help="Subscription id of the cluster",
     )
-    
+
     parser_setup.add_argument(
         "--rg",
         type=str,
         required=True,
         help="Resource group of the cluster",
     )
-    
+
     parser_setup.add_argument(
         "--location",
         type=str,
         required=True,
         help="Location of the cluster",
     )
-    
+
     parser_setup.add_argument(
         "--client_id",
         type=str,
         required=True,
         help="Service principal client id",
     )
-    
+
     parser_setup.add_argument(
         "--client_secret",
         type=str,
         required=True,
         help="Service Principal client secret",
     )
-    
+
     parser_setup.add_argument(
         "--storage_account_name",
         type=str,
         required=True,
         help="Pet blob storage secrets",
     )
-    
+
     parser_setup.add_argument(
         "--storage_account_key",
         type=str,
         required=True,
         help="Pet blob storage secrets",
     )
-    
+
     parser_setup.add_argument(
         "--docker_server",
         type=str,
         required=True,
         help="Docker server",
     )
-    
+
     parser_setup.add_argument(
         "--docker_username",
         type=str,
         required=True,
         help="Docker username",
     )
-    
+
     parser_setup.add_argument(
         "--docker_password",
         type=str,
         required=True,
         help="Docker password",
     )
-    
+
     parser_setup.set_defaults(func=setup)
-    
-     # ---------------------------------- #
-     #        CONFIGURE JOB YAML          #
-     # ---------------------------------- #
-    
+
+    # ---------------------------------- #
+    #        CONFIGURE JOB YAML          #
+    # ---------------------------------- #
+
     parser_configure = subparser.add_parser(
         "configure", help="Generate yaml job file"
     )
@@ -185,25 +182,24 @@ if __name__ == '__main__':
         "--size",
         type=int,
         required=False,
-        help="number  of worker hosts to start the job with",
+        help="set size to automatically set min_size = max_size = size",
     )
     parser_configure.set_defaults(func=configure)
-    
-    
-     # ---------------------------------- #
-     #              RUN JOB               #
-     # ---------------------------------- #
-    
+
+    # ---------------------------------- #
+    #              RUN JOB               #
+    # ---------------------------------- #
+
     parser_run_job = subparser.add_parser(
         "run_job", help="Run your training job"
     )
-    
+
     parser_run_job.set_defaults(func=run_job)
-    
+
     # -----------------------------------------
-    
+
     args = parser.parse_args()
-    
+
     if args.command == "setup":
         setup(args)
     elif args.command == "configure":
