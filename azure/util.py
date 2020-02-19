1b3392d217391de218b2cd74b504565300f7b1ce
Ravi shankar Kolli <rakoll@microsoft.com>
2020-02-19 08:15:35 -0800

    Fixed a typo in configure_json

--- azure/util.py
+++ azure/util.py
@@ -48,7 +48,7 @@ def configure_json(args):
     data["properties"]["masterProfile"]["count"] = 1
     data["properties"]["agentPoolProfiles"][0]["count"] = args.min_size
     data["properties"]["masterProfile"]["vmSize"] = args.master_vm
-    data["properties"]["agentPoolProfiles"]["vmSize"] = args.worker_vm
+    data["properties"]["agentPoolProfiles"][0]["vmSize"] = args.worker_vm
     
     json.dump(data, open(result_json_file,"w"), indent=4)
 
