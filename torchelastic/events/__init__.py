e049faa9a98e1f5ecad51a0f0d7a0f63bb35f867
Aliaksandr Ivanou <aivanou@fb.com>
2020-04-23 10:12:17 -0700

    Implement event handler and scuba event handler for capturing agent state
    Summary: The diff introduces the event module that devs that use to record the events and build event handlers.
    
    Reviewed By: kiukchung
    
    Differential Revision: D20828137
    
    fbshipit-source-id: 2523afb17a0cc63ae6954d8392c0dadc99c28989

---
+++ torchelastic/events/__init__.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env/python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Events API
+
+**Overview**:
+
+The event API is used to publish and process events. The module can be used
+to capture state transitions to debug or give insight into the execution
+flow. The :py:class:`torchelastic.events.Event` is an object containing information
+about an occurrence during the execution of the program.
+
+
+.. note:: The event type ``torchelastic`` is reserved by torchelastic for
+          platform level events that can be produced by the agent or worker.
+
+
+**Record Events**:
+
+The event module resembles python's logging framework in terms
+of usage and consists of two parts: handler configuration and event publishing.
+
+The example below shows the simple event publishing mechanism.
+
+::
+
+  from torchelastic.events import Event, record_event
+
+  event.configure(event.NullEventHandler()) # uses event_type = "torchelastic"
+  event.configure(event.ConsoleEventHandler(), event_type = "foo")
+
+  def execute():
+    event = Event(name="test_event", event_type="foo")
+    # The code below will be processed by the ConsoleEventHandler
+    record_event(event)
+
+Another way of using the module is via
+:py:meth:`torchelastic.events.record`.
+
+::
+
+  from torchelastic.events import record
+
+  def execute():
+    metadata = {'key':'value'}
+    record(event_name="test", event_type="console", metadata=metadata)
+
+
+**Writing a Custom Event Handler**:
+
+The custom event handler can be implemented by extending
+:py:class:`torchelastic.events.EventHandler` class.
+
+Example
+
+::
+
+  from torchelastic.events import EventHandler
+
+  class StdoutEventHandler(EventHandler):
+     def record(self, event):
+         print(f"[{event.event_type}]: event_name: {event.name}")
+
+  event.configure(StdoutEventHandler(), event_type="stdout_events")
+
+Now all events with event_type 'stdout_events' will be printed to stdout.
+
+"""
+
+from .api import (  # noqa F401
+    ConsoleEventHandler,
+    Event,
+    EventHandler,
+    configure,
+    record,
+    record_event,
+)
---
+++ torchelastic/events/api.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import abc
+import time
+from typing import Any, Dict
+
+
+class Event:
+    """
+    The class represents the generic event that occurs during the torchelastic
+    job execution. The event can be any kind of meaningful action.
+
+    Args:
+        name: event name.
+        type: type of event, determines what handler will process the event.
+        metadata: additional data that is associated with the event.
+        timestamp: timestamp in milliseconds when event occured.
+    """
+
+    __slots__ = ["name", "type", "metadata", "timestamp"]
+
+    def __init__(
+        self,
+        name: str,
+        type: str = "torchelastic",
+        # Cannot use optional due to https://github.com/facebook/pyre-check/issues/197
+        # pyre-fixme[9]: metadata has type `Dict[str, Any]`; used as `None`.
+        metadata: Dict[str, Any] = None,
+        timestamp: int = 0,
+    ):
+        self.name = name
+        self.type = type
+        self.metadata = metadata
+        self.timestamp = timestamp
+
+
+class EventHandler(abc.ABC):
+    """
+    Event handler is responsible for capturing and recording events to the different
+    destinations, e.g. stdout, file, etc.
+    """
+
+    @abc.abstractmethod
+    def record(self, event: Event):
+        raise NotImplementedError()
+
+
+class ConsoleEventHandler(EventHandler):
+    """
+    The event handler that prints the captured events to the console.
+    """
+
+    def record(self, event: Event):
+        print(f"[{event.timestamp}] {event.type}:{event.name}")
+
+
+class NullEventHandler(EventHandler):
+    """
+    The default event handler that does nothing.
+    """
+
+    def record(self, event: Event):
+        pass
+
+
+_events_map: Dict[str, EventHandler] = {}
+_default_event_handler: EventHandler = NullEventHandler()
+
+
+def _getEventHandler(event_type: str):
+    if event_type in _events_map:
+        return _events_map[event_type]
+    return _default_event_handler
+
+
+def configure(handler: EventHandler, event_type: str = ""):
+    if not event_type:
+        global _default_event_handler
+        _default_event_handler = handler
+    else:
+        _events_map[event_type] = handler
+
+
+def record_event(event: Event):
+    r"""
+    Records the event to the destination configured by the event.type parameter.
+
+    Args:
+        event (Event): Event to be recorded
+    """
+    if event.metadata is None:
+        event.metadata = {}
+    _getEventHandler(event.type).record(event)
+
+
+def record(
+    event_name: str,
+    event_type: str = "torchelastic",
+    # Cannot use optional due to https://github.com/facebook/pyre-check/issues/197
+    # pyre-fixme[9]: metadata has type `Dict[str, Any]`; used as `None`.
+    metadata: Dict[str, Any] = None,
+):
+    r"""
+    Constructs and  records the event to the destination
+    configured by the event.type parameter.
+
+    Args:
+        event_name: The name of the event
+        event_type: The type of the event. Is used to determine what handler
+            will process the event
+        metadata: Arbitrary data that is associated with the event.
+    """
+
+    if metadata is None:
+        metadata = {}
+    event = Event(
+        event_name, event_type, metadata, timestamp=int(round(time.time() * 1000))
+    )
+    record_event(event)
