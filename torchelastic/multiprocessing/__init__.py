4caabf3942f1db178021a3855cf1a91f9853cc82
Aliaksandr Ivanou <aivanou@fb.com>
2020-10-02 16:57:11 -0700

    Remove custom spawn fork
    Summary: Remove custom spawn fork since we modified original spawn and introduced error classification.
    
    Reviewed By: taohe
    
    Differential Revision: D24000062
    
    fbshipit-source-id: ba73cb4ae00164dae3a3098589baa74059cc4285

--- torchelastic/multiprocessing/__init__.py
+++ torchelastic/multiprocessing/__init__.py
@@ -5,22 +5,3 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
-
-"""
-torchelastic.multiprocessing is a wrapper around the native :mod:`multiprocessing`
-module. It registers built in or custom error reporter, that enables error capturing
-across subprocesses and faciliates error propagation in the parent process
-
-Added helper function to spawn N processes and wait for completion of any of
-them. This depends `mp.get_context` which was added in Python 3.4.
-"""
-# @manual=//caffe2:utils_internal
-from torch._utils_internal import get_file_path, prepare_multiprocessing_environment
-
-
-def _initialize():
-    # Setup LD_LIBRARY_PATH for subprocesses
-    prepare_multiprocessing_environment(get_file_path("torch"))
-
-
-_initialize()
