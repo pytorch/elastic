7e1f0acc79eb660e22f212ba3a9c3bb7270831c7
Aliaksandr Ivanou <aivanou@fb.com>
2021-04-25 16:52:42 -0700

    Torchelastic bring back setup.py
    Summary: Restore setup.py that requires for building docs.
    
    Reviewed By: kiukchung
    
    Differential Revision: D27974755
    
    fbshipit-source-id: 2c2ecd9f9b4a9cd0f3415991ff05270ebb6ef195

--- setup.py
+++ setup.py
@@ -7,7 +7,54 @@
 # LICENSE file in the root directory of this source tree.
 
 
+import sys
+
+from setuptools import find_packages, setup
+
+
+def get_version():
+    return "0.2.3.dev0"
+
+
 if __name__ == "__main__":
-    raise RuntimeError(
-        "Torchelastic got merged with pytorch. Use pytorch: https://pytorch.org/"
+    if sys.version_info < (3, 8):
+        sys.exit("python >= 3.8 required for torchelastic")
+
+    with open("README.md", encoding="utf8") as f:
+        readme = f.read()
+
+    with open("requirements.txt") as f:
+        reqs = f.read()
+
+    version = get_version()
+    print("-- Building version: " + version)
+
+    setup(
+        # Metadata
+        name="torchelastic",
+        version=version,
+        author="PyTorch Elastic Devs",
+        author_email="torchelastic@fb.com",
+        description="PyTorch Elastic Training",
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        url="https://github.com/pytorch/elastic",
+        license="BSD-3",
+        keywords=["pytorch", "machine learning", "elastic", "distributed"],
+        python_requires=">=3.8",
+        install_requires=reqs.strip().split("\n"),
+        include_package_data=True,
+        packages=find_packages(exclude=("*.test", "aws*", "*.fb")),
+        test_suite="torchelastic.tsm.test.suites.unittests",
+        # PyPI package information.
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.8",
+            "Topic :: System :: Distributed Computing",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        ],
     )
