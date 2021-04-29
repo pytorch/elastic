3431ef76b3c858b78a6bb73757c9227db1925406
Aliaksandr Ivanou <aivanou@fb.com>
2021-04-29 11:02:54 -0700

    Rename torch.distributed.elastic_launch to torch.distributed.run (#56831)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56831
    
    Rename torch.distributed.elastic_launch to torch.distributed.run
    
    Reviewed By: kiukchung
    
    Differential Revision: D27921159
    
    fbshipit-source-id: cc7f2f035223b2d4abd7373af298998887e14c12

--- torchelastic/distributed/launch.py
+++ torchelastic/distributed/launch.py
@@ -6,7 +6,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from torch.distributed.elastic_launch import parse_args, run
+from torch.distributed.run import parse_args, run
 
 
 def main(args=None):
